8c869e49c63023c663af6afa3ca2da69d7d20dba
Dalengo117 <108893820+Dalengo117@users.noreply.github.com>
2022-07-25 13:27:38 -0500

    Update graficaLinear.js

--- js/graficaLinear.js
+++ js/graficaLinear.js
@@ -4,8 +4,8 @@ const myChart = new Chart(ctx, {
     data: {
         labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado','Domingo'],
         datasets: [{
-            label: 'Dinero gastado',
-            data: [12, 19, 3, 5, 2, 3,30],
+            label: 'Esta semana',
+            data: [120000, 190000, 300000, 500000, 220000, 350000,200000],
             backgroundColor: [
                 'rgb(153, 102, 255)',
                 'rgb(54, 162, 235)',
@@ -17,7 +17,24 @@ const myChart = new Chart(ctx, {
             ],
             borderColor: 'rgb(153, 102, 255)',
             borderWidth: 1
-        }]
+        },
+        {
+            label: 'Semana pasada',
+            data: [300000, 120000, 100000, 50000, 520000, 750000,700000],
+            backgroundColor:  [
+                'rgb(54, 162, 235)',
+                'rgb(203, 25, 10)',
+                'rgb(255, 99, 132)',
+                'rgb(255, 206, 86)',
+                'rgb(118, 247, 5)',
+                'rgb(255, 159, 64)',
+                'rgb(118, 247, 5)',
+                
+            ],
+            borderColor:'rgb(203, 25, 10)',
+          }
+      
+    ]
     },
     options: {
         scales: {
